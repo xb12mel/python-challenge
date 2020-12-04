a75c37e4dbc5e91af0670d90249fe09079d29dee
Melvin Byrd <xb12mel@gmail.com>
2020-12-03 23:33:00 -0500

    Adding folders from homework assignment

---
+++ PyBank/main.py
@@ -0,0 +1,14 @@
+# This challenge is to create a script to analyze the company's financial records.
+# We need two columns: Date and Profit/Loss
+
+# Total number of months included in the dataset
+
+# The net total amount of "Profit/Losses" over the entire period
+
+# Calculate the changes in "Profit/Losses" ovver the entire period, then find the average of those changes
+
+# The greatest increas in profits (date and amount)
+
+# The greatest decrease in losses (date and amount)
+
+# Needs to print the analysis to the screen and exort a txt file with results.
